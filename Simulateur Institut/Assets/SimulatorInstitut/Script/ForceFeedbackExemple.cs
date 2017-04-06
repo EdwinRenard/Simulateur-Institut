94cdc684aa60f3c9f5a583d48c34af9fab923a3b
EdwinRenard <edre89@gmail.com>
2017-04-06 17:09:21 +0200

    Modification de l'agencement du dossier.

---
+++ Simulateur Institut/Assets/SimulatorInstitut/Script/ForceFeedbackExemple.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//Example of force feedback implementation.
+//Here we will set up a system for the steering wheel to return to its initial position.
+//For this purpose, the angle of the steering wheel is recovered and an opposing force proportional to the angle of the steering wheel is applied.
+public class ForceFeedbackExemple : MonoBehaviour {
+	ForceFeedbackInterface myForceFeedback; //1) Our force feedback management class.
+
+	void Awake()
+	{
+		myForceFeedback = new ForceFeedbackInterface(); //2) Initialization of the force feedback.
+	}
+
+	void Update(){//3) Application of the effect. Here the effect must be applied continuously, that is why it is in the Update.
+		float axeVolant = TeamUtility.IO.InputManager.GetAxisRaw ("Horizontal");
+
+		int force = Mathf.FloorToInt(10000 * axeVolant);
+		myForceFeedback.SetDeviceForces (force, 0);
+	}
+
+	void OnApplicationQuit(){
+		myForceFeedback.shutDownForceFeedback();//4) The device is released with force feedback.
+
+	}
+}
---
+++ Simulateur Institut/Assets/SimulatorInstitut/Script/ForceFeedbackInterface.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class ForceFeedbackInterface {
+
+	[DllImport("user32")]
+	private static extern int GetForegroundWindow();			//To access the windows affected by force feedback.
+	[DllImport("DirectInputLibrary")]
+	public static extern int InitDirectInput();					//Initialize the device.
+	[DllImport("DirectInputLibrary")]
+	public static extern int InitForceFeedback(int HWND);		//Initializes the force feedback of the device.
+	[DllImport("DirectInputLibrary")]
+	public static extern int SetDeviceForcesXY(int x, int y);	//Gives an action force on the X axis and / or the Y axis.
+	[DllImport("DirectInputLibrary")]
+	public static extern int StartEffect();						//Starts the initialized effect.
+	[DllImport("DirectInputLibrary")]
+	public static extern int StopEffect();						//Stops the current effect.
+	[DllImport("DirectInputLibrary")]
+	public static extern int DetectForceFeedbackDevice();		//Device detection compatible with force feedback.
+	[DllImport("DirectInputLibrary")]
+	public static extern int FreeForceFeedback();				//Releases the force feedback.
+	[DllImport("DirectInputLibrary")]
+	public static extern int FreeDirectInput();					//Releases the device.
+
+	//Variables
+	public int forceX { get; private set; }
+	public int forceY { get; private set; }
+	public bool forceFeedbackEnabled {get; private set;}
+
+	/// <summary>
+	/// Constructor of the class to handle the force feedback.
+	/// </summary>
+	public ForceFeedbackInterface()
+	{
+		InitialiseForceFeedback ();
+	}
+		
+	/// <summary>
+	/// Initializes the force feedback.
+	/// </summary>
+	private void InitialiseForceFeedback() 
+	{ 
+		if(forceFeedbackEnabled)
+		{
+			Debug.LogWarning("Force feedback attempted to initialise but was already running !");
+			return;
+		}
+
+		//We retrieve the ID of the window that requires the force feedback.
+		int hwnd = GetForegroundWindow();
+		//Call to the function of the DLL to initialize the force feedback.
+		if(InitForceFeedback(hwnd) >= 0){
+			StartEffect();
+			forceFeedbackEnabled = true;
+		}
+	}
+
+	/// <summary>
+	/// Force feedback reset function.
+	/// </summary>
+	public void ResetForceFeedback()
+	{
+		FreeForceFeedback();
+		forceFeedbackEnabled = false;
+		forceX = forceY = 0;
+	}
+
+	/// <summary>
+	/// Full force feedback stop function. Stop all effects in progress and releases the force feedback.
+	/// </summary>
+	public void shutDownForceFeedback()
+	{
+		if(forceFeedbackEnabled)
+		{
+			StopEffect();
+		}
+		FreeDirectInput();
+	}
+
+	/// <summary>
+	/// Function to give a certain force on the X and Y axes.
+	/// </summary>
+	public void SetDeviceForces(int forceX, int forceY)
+	{
+		this.forceX = forceX;
+		this.forceY = forceY;
+
+		int FFdetected = DetectForceFeedbackDevice();
+
+		if (FFdetected >= 0 && !forceFeedbackEnabled) {
+			InitialiseForceFeedback();
+			forceFeedbackEnabled = true;
+		}
+
+		if (forceFeedbackEnabled)
+		{
+			if(FFdetected < 0) {
+				//If the device is disconnected.
+				StopEffect ();
+				FreeForceFeedback ();
+				forceFeedbackEnabled = false;
+
+			} else {
+				SetDeviceForcesXY(forceX, forceY);
+			}
+		}
+	}
+}
